6ee45f30d10c2a144fca50bacacb369f7f95aa0d
alizarazot <alizarazot@ufpso.edu.co>
2024-08-23 08:04:32 -0500

    Add progress of Restaurant

--- 2308-Restaurant/src/main/java/local/restaurant/Menu.java
+++ 2308-Restaurant/src/main/java/local/restaurant/Menu.java
@@ -42,4 +42,82 @@ public class Menu {
             }
         }
     }
+
+    public int getTotalSalesPlateWeek(int plate) {
+        int total = 0;
+
+        for (int day = 0; day < Menu.DAYS.length; day++) {
+            total += data[day][plate];
+        }
+
+        return total;
+    }
+
+    public int getDayMoreSalesPlate(int plate) {
+        int bestDay = 0;
+
+        for (int day = 0; day < Menu.DAYS.length; day++) {
+            if (data[day][plate] > data[bestDay][plate]) {
+                bestDay = day;
+            }
+        }
+
+        return bestDay;
+    }
+
+    public int getDayLessSalesPlate(int plate) {
+        int worstDay = 0;
+
+        for (int day = 0; day < Menu.DAYS.length; day++) {
+            if (data[day][plate] < data[worstDay][plate]) {
+                worstDay = day;
+            }
+        }
+
+        return worstDay;
+    }
+
+    public int getDaySales(int day) {
+        int total = 0;
+
+        for (int plate = 0; plate < plates.length; plate++) {
+            total += plates[day][plate];
+        }
+
+        return total;
+    }
+
+    public int getDayMostSales() {
+        int bestDay = 0;
+
+        for (int day = 0; day < Menu.DAYS.length; day++) {
+            if (this.getDaySales(day) > this.getDaySales(bestDay)) {
+                bestDay = day;
+            }
+        }
+
+        return bestDay;
+    }
+
+    public int getDayLessSales() {
+        int worstDay = 0;
+
+        for (int day = 0; day < Menu.DAYS.length; day++) {
+            if (this.getDaySales(day) < this.getDaySales(worstDay)) {
+                worstDay = day;
+            }
+        }
+
+        return worstDay;
+    }
+
+    public int getAverageSalesDay() {
+        int total = 0;
+
+        for (int day = 0; day < Menu.DAYS.length; day++) {
+            total += getDaySales(day);
+        }
+
+        return total / Menu.DAYS.length;
+    }
 }
